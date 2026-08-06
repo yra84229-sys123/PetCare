ff90b07dad921dbb7d2d4020a850248d6852acbb
Yura <yra84229@gmail.com>
2026-08-06 16:18:50 +0700

    Update Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/PetCare/",   // important for GitHub Pages
+  base: '/PetCare/',
   plugins: [react()],
 })
